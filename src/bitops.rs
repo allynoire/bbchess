44ab79168a012e59c442159d0ed10fb82b27d0fb
allynoire <61152193+allynoire@users.noreply.github.com>
2026-09-24 00:59:36 +0200

    calculate ray attack

--- src/bitops.rs
+++ src/bitops.rs
@@ -1,30 +1,23 @@
-/// returns the index of the first set bit from the least significant digit
-pub fn bitscan(bb: u64) -> u64 {
-    assert!(bb != 0);
-    let mut i = 0;
-    while bb & (1 << i) == 0 { i += 1 };
-    i
+
+pub trait BitOps {
+    fn pop_highest_one(&mut self) -> Option<u32>;
+    fn pop_lowest_one(&mut self) -> Option<u32>;
 }
 
-/// returns the index of the first set bit from the most significant digit
-pub fn bitscanr(bb: u64) -> u64 {
-    assert!(bb != 0);
-    let mut i = 63;
-    while bb & (1 << i) == 0 { i -= 1 };
-    i
+impl BitOps for u64 {
+    fn pop_highest_one(&mut self) -> Option<u32> {
+        if let Some(i) = self.highest_one() {
+            *self &= !(1 << i);
+            Some(i)
+        }
+        else { None }
+    }
+    
+    fn pop_lowest_one(&mut self) -> Option<u32> {
+        if let Some(i) = self.lowest_one() {
+            *self &= !(1 << i);
+            Some(i)
+        }
+        else { None }
+    }
 }
-
-/// clears the first set bit from the least significant digit returning its index
-pub fn popbit(bb: &mut u64) -> u64 {
-    let i = bitscan(*bb);
-    *bb &= !(1 << i);
-    i
-}
-
-/// clears the first set bit from the most significant digit returning its index
-pub fn popbitr(bb: &mut u64) -> u64 {
-    let i = bitscanr(*bb);
-    *bb &= !(1 << i);
-    i
-}
-
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,75 @@ use std::{cmp::min, ops::{Deref, DerefMut}, str::Lines, sync::Arc};
 use bitboards::bitops::*;
 
 
+/*
+8  38 39 3a 3b 3c 3d 3e 3f 
+7  30 31 32 33 34 35 36 37 
+6  28 29 2a 2b 2c 2d 2e 2f 
+5  20 21 22 23 24 25 26 27 
+4  18 19 1a 1b 1c 1d 1e 1f 
+3  10 11 12 13 14 15 16 17 
+2  08 09 0a 0b 0c 0d 0e 0f 
+1  00 01 02 03 04 05 06 07
+
+   a  b  c  d  e  f  g  h
+
+*/
 
 fn main() {
+
+
+    let mut occ = 0u64
+        | 1 << 0x20
+        | 1 << 0x38
+        | 1 << 0x30 
+        | 1 << 0x31 
+        | 1 << 0x2a 
+        | 1 << 0x2b 
+        | 1 << 0x3e;
+
+        
+        dump_bb(occ, 0xff);
+        // dump_bb(ray, 0x00);
+        
+    // ray hit
+    for i in 0..8 {
+        let ray = ray_up(i);
+        let hit: u64 = (occ & ray).lowest_one().map(|j| 1 << j).unwrap_or(0);
+        // dump_bb(hit, 0xff);
+    }
+
+
+    // ray until and with hit
+    for i in 0x20..0x28 {
+        let mut ray = ray_up(i);
+        if let Some(i) = (occ & ray).lowest_one() {
+            while let Some(j) = ray.highest_one() && j > i {
+                ray.pop_highest_one();
+            }
+        }
+        dump_bb(ray, i);
+    }
+
+
+
 
     let rook_atk = atk_table(rook_atk_bb);
     let bish_atk = atk_table(bish_atk_bb);
     let quee_atk = atk_table(|i| rook_atk_bb(i) & bish_atk_bb(i));
 
-    dump_table(&rook_atk);
+    let ray_up = atk_table(ray_up);
+
+    // dump_table(&ray_up);
+}
+
+fn dump_pos() {
+    for y in 0..8 {
+        for x in 0..8 {
+            let i = x + (7-y) * 8;
+            print!("{i:02x} ")
+        }
+        println!()
+    }
 }
 
 fn dump_bb(bb: u64, pos: usize) {
@@ -71,6 +132,14 @@ where F: Fn(usize) -> u64
     for i in 0..64 { table[i] = f(i) }
     table
 }
+
+
+fn ray_up(i: usize) -> u64 {
+    let mut bb = 0;
+    bb |= 0x0101010101010100 << (i % 8) << (i & !0x7);
+    bb
+}
+
 
 
 /// computes the rook attacks for the given position
